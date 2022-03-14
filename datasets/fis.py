d511a0fdfa580f2287d07aba0f118312db279d68
2ailesB <leboudec.lise@gmail.com>
2022-03-14 23:32:53 +0100

    feat/add net, metrics, training loop

--- datasets/fis.py
+++ datasets/fis.py
@@ -1,5 +1,6 @@
 from cmath import pi
 import torch
+import matplotlib.pyplot as plt
 
 
 def f1(x, a):
@@ -22,3 +23,35 @@ def f4(x, a1, a2, a3):
 def f5(x, a1, a2, a3):
     xmpi = torch.abs(x - pi)
     return (0.5 * a1 * xmpi ** 2 - a1 * a3) * (xmpi <= a3) + (a2 * xmpi ** 2 - a2 * a3 - 0.5 * a3 ** 2 * (a1 - a2)) * (xmpi > a3)
+
+if __name__ == "__main__":
+    x=torch.arange(0, 2*pi, 0.1)
+    fig = plt.figure(figsize=(10, 10))
+    plt.subplot(511)
+    plt.plot(x, f1(x, 1))
+    plt.xlabel('x')
+    plt.ylabel('f1(x)')
+    plt.title('f1(x)')
+    plt.subplot(512)
+    plt.plot(x, f2(x, 1))
+    plt.xlabel('x')
+    plt.ylabel('f2(x)')
+    plt.title('f2(x)')
+    plt.subplot(513)
+    plt.plot(x, f3(x, 1, 2, 0.5))
+    plt.xlabel('x')
+    plt.ylabel('f3(x)')
+    plt.title('f3(x)')
+    plt.subplot(514)
+    plt.plot(x, f4(x, 1, 2, 0.5))
+    plt.xlabel('x')
+    plt.ylabel('f4(x)')
+    plt.title('f4(x)')
+    plt.subplot(515)
+    plt.plot(x, f5(x, 1, 2, 0.5))
+    plt.xlabel('x')
+    plt.ylabel('f5(x)')
+    plt.title('f5(x)')
+    plt.show()
+    plt.savefig('results/functionsf1-5.png')
+    plt.close()
---
+++ training.py
@@ -0,0 +1,58 @@
+import torch
+import torch.functional as F
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+
+
+# data
+train_dataloaeder = None
+test_dataloaeder = None
+
+# model
+K = 4
+h1, h2, h3 = 16, 16, 16
+indim = 1
+outdim = 4
+alpha = 1
+lbd = 0.1
+model = nn.Sequential(nn.Linear(indim, h1), nn.ELU(alpha), 
+        nn.Dropout(lbd),
+        nn.Linear(h1, h2), nn.ELU(alpha), 
+        nn.Linear(h2, h3), nn.ELU(alpha), 
+        nn.Linear(h3, outdim), nn.Softmax())
+
+# training procedure
+lr = 0.001
+epochs = 1000
+path = 'results/'
+verbose = 10
+optim = torch.optim.Adam(model.parameters(), lr = lr)
+loss = torch.nn.CrossEntropyLoss()
+writer = SummaryWriter(path)
+
+#learing loop
+for epoch in range(epochs):
+    epoch_l = []
+    epoch_ltest = []
+    for X, y in train_dataloaeder:
+        yhat = model(X)
+        optim.zero_grad()
+        l = loss(y, yhat)
+        l.backward()
+        optim.step()
+        epoch_l.append(l)
+
+    writer.add_scalars('loss', epoch_l.mean(), epoch)
+
+    if epoch % verbose:
+        print(f'training loss at epoch {epoch} is {epoch_l.mean()}')
+        print(f'training accuracy at epoch {epoch} is {epoch_l.mean()}') # TODO:compute accuracy : see utils
+
+    for X, y in test_dataloaeder:
+        yhat = model(X)
+        ltest = loss(y, yhat)
+        epoch_ltest.append(ltest)
+
+    if epoch % verbose:
+        print(f'testing loss at epoch {epoch} is {epoch_l.mean()}')
+        print(f'testing accuracy at epoch {epoch} is {epoch_l.mean()}')
